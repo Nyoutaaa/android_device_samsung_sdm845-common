3e5b547fb4e24e929cf2e0d7c79bfa93560be2ef
SamuelSouzaa <shhsdsjshs@gmail.com>
2021-02-05 14:56:01 -0300

    sdm845-common: Revert Build Trust HAL

--- common.mk
+++ common.mk
@@ -110,10 +110,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.touch@1.0-service.sdm845
 
-# Trust
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
-
 # Power
 PRODUCT_PACKAGES += \
     power.qcom
